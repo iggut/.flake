38d404ab520cedf5e5d09d26c4156bbcdd6ab472
Igor G <igor.gutchin@gmail.com>
2023-07-15 18:21:43 -0400

    nvidia

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -1,73 +1,34 @@
-{ config, pkgs, lib, inputs, user, ... }:
 {
-
-#Nvidia
+  config,
+  pkgs,
+  lib,
+  inputs,
+  user,
+  ...
+}: {
+  #Nvidia
   #Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-
   services.xserver = {
-    videoDrivers = [ "nvidia" ];
-
-    config = ''
-      Section "Device"
-          Identifier  "Intel Graphics"
-          Driver      "intel"
-          #Option      "AccelMethod"  "sna" # default
-          #Option      "AccelMethod"  "uxa" # fallback
-          Option      "TearFree"        "true"
-          Option      "SwapbuffersWait" "true"
-          BusID       "PCI:0:2:0"
-          #Option      "DRI" "2"             # DRI3 is now default
-      EndSection
-
-      Section "Device"
-          Identifier "nvidia"
-          Driver "nvidia"
-          BusID "PCI:1:0:0"
-          Option "AllowEmptyInitialConfiguration"
-          Option         "TearFree" "true"
-      EndSection
-    '';
-    screenSection = ''
-      Option         "metamodes" "nvidia-auto-select +0+0 {ForceFullCompositionPipeline=On}"
-      Option         "AllowIndirectGLXProtocol" "off"
-      Option         "TripleBuffer" "on"
-      '';
-    deviceSection = '' 
-    Option "TearFree" "true"
-    '';
+    videoDrivers = ["nvidia"];
   };
 
-  hardware.opengl.enable = true;
-  hardware.opengl.driSupport = true;
-  hardware.opengl.driSupport32Bit = true;
-  hardware.nvidia.nvidiaSettings = true;
-  hardware.nvidia.powerManagement.enable = true;
-  hardware.nvidia.forceFullCompositionPipeline = true;
-
-  # Cuda
-  services.xmr-stak.cudaSupport = true; 
-  # Optionally, you may need to select the appropriate driver version for your specific GPU.
-  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.latest;
-  # nvidia-drm.modeset=1 is required for some wayland compositors, e.g. sway
-  hardware.nvidia.modesetting.enable = true;
-  #Switch GPU
-  #services.switcherooControl.enable = true;
-
-
-  #hardware.nvidia.prime = {
-    # Sync Mode
-    #reverseSync.enable = true;
-    # Offload Mode
-    #offload.enable = true;
-
-    # Bus ID of the NVIDIA GPU. You can find it using lspci, either under 3D or VGA
-    #nvidiaBusId = "PCI:1:0:0";
-
-    # Bus ID of the Intel GPU. You can find it using lspci, either under 3D or VGA
-    #intelBusId = "PCI:0:2:0";
-  #};
-
-
+  hardware = {
+    nvidia = {
+      powerManagement.enable = true;
+      modesetting.enable = true;
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
+    };
+    opengl = {
+      enable = true;
+      extraPackages = with pkgs; [
+        nvidia-vaapi-driver
+      ];
+    };
+    opentabletdriver = {
+      enable = true;
+      daemon.enable = true;
+    };
+  };
 }
