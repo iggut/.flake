55331c693d7d99d344e1cb9212afbd114e00aaa3
Igor G <igor.gutchin@gmail.com>
2023-07-18 11:03:05 -0400

    lap prime test

--- modules/nvidia-laptop.nix
+++ modules/nvidia-laptop.nix
@@ -5,7 +5,28 @@
   inputs,
   user,
   ...
-}: {
+}: let
+  nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
+    export __NV_PRIME_RENDER_OFFLOAD=1
+    export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+    export __GLX_VENDOR_LIBRARY_NAME=nvidia
+    export __VK_LAYER_NV_optimus=NVIDIA_only
+    exec -a "$0" "$@"
+  '';
+in {
+  environment.systemPackages = with pkgs; [
+    nvidia-offload
+  ];
+
+  hardware.nvidia.nvidiaSettings = false;
+
+  environment.variables = {
+    LIBVA_DRIVER_NAME = "iHD";
+    VDPAU_DRIVER = "va_gl";
+  };
+
+  virtualisation.docker.enableNvidia = true;
+  virtualisation.podman.enableNvidia = true;
   #Nvidia
   #Allow unfree packages
   nixpkgs.config.allowUnfree = true;
@@ -22,14 +43,16 @@
 
   hardware = {
     nvidia = {
+      nvidiaPersistenced = true;
       powerManagement.enable = true;
+      powerManagement.finegrained = true;
       # Modesetting is needed for most wayland compositors
       modesetting.enable = true;
       # Use the open source version of the kernel module
       # Only available on driver 515.43.04+
       open = true;
-      # Enable the nvidia settings menu
-      nvidiaSettings = true;
+      # Enable the nvidia settings menu < nvidia-settings doesn't work with clang lto
+      nvidiaSettings = false;
       # Optionally, you may need to select the appropriate driver version for your specific GPU.
       package = config.boot.kernelPackages.nvidiaPackages.stable;
       prime = {
